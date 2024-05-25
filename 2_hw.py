3df377ba9f03f570df5830c43c99c03425b3cd82
home_work <mr.ivan.ivanovich777@mail.ru>
2024-05-25 15:28:09 +0300

    Update 2_hw.py

--- 2_hw.py
+++ 2_hw.py
@@ -1,1 +1,25 @@
-task_1():
+a = 2
+print(a, "относится к типу", type(a))
+
+b = 35.53
+print(b, "относится к типу", type(b))
+
+
+g = 'Сторона'
+print (g + '\n')
+
+
+test_list = ['один', 'два', 'три', 'четыре', 'пять']
+for elem in test_list:
+  print(elem)
+
+
+print(15 > 8)
+print(12 == 3)
+print(4 < 2)
+
+b = False
+if a:
+    print('a = True')
+else:
+    print("a != True")
